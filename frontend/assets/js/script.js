661b34e787025f92b00f7c304f14a2ca91b7977f
elbert1509 <a.elbert1509@gmail.com>
2025-03-15 22:55:27 +0100

    maj 2024

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,15 +1,42 @@
+
+
+let page = window.location.pathname.split("/").pop();
+console.log(page)
+let membres; 
+
 // Données des membres avec leur photo et cotisations
-const membres = [
-    { nom: "Waza", photo: "frontend/assets/images/photo1.png", cotisations: [5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { nom: "Melissa", photo: "frontend/assets/images/melissa.jpg", cotisations: [5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { nom: "Victoire", photo: "frontend/assets/images/stephanie.jpg", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { nom: "Isis", photo: "frontend/assets/images/isis.jpg", cotisations:[5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { nom: "Norbert", photo: "frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { nom: "Bolingo", photo: "frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { nom: "Evan's", photo: "frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { nom: "Tic-Tac ", photo: "frontend/assets/images/photo1.png", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { nom: "Naz-K", photo: "frontend/assets/images/nazk.jpg", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
-];
+
+    if (page == "index.html"){
+
+         membres = [
+            { nom: "Waza", photo: "/frontend/assets/images/waza.jpg", cotisations: [5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Melissa", photo: "/frontend/assets/images/melissa.jpg", cotisations: [5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Victoire", photo: "/frontend/assets/images/stephanie.jpg", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Isis", photo: "/frontend/assets/images/isis.jpg", cotisations:[5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Norbert", photo: "/frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Bolingo", photo: "/frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Evan's", photo: "/frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Tic-Tac ", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Naz-K", photo: "/frontend/assets/images/nazk.jpg", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
+        ];
+    }else if (page == "2024.html"){
+
+         membres = [
+            { nom: "Waza", photo: "/frontend/assets/images/waza.jpg", cotisations: [5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000] },
+            { nom: "Melissa", photo: "/frontend/assets/images/melissa.jpg", cotisations: [5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000] },
+            { nom: "Victoire", photo: "/frontend/assets/images/stephanie.jpg", cotisations: [5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000] },
+            { nom: "Isis", photo: "/frontend/assets/images/isis.jpg", cotisations:[5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000]  },
+            { nom: "Norbert", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Bolingo", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 5000, 150, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Evan's", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000]  },
+            { nom: "Tic-Tac ", photo: "/frontend/assets/images/photo1.png", cotisations:[5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000] },
+            { nom: "Naz-K", photo: "/frontend/assets/images/nazk.jpg", cotisations: [5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Bagio", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
+        ];
+    }
+
+
+
 
 // Liste des mois
 const mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
