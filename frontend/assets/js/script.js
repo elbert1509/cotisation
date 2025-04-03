ccad5dc0720cbc628d9cff92fe5b19302c39651f
elbert1509 <105393405+elbert1509@users.noreply.github.com>
2025-04-03 16:04:34 +0200

    Update script.js

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -116,7 +116,7 @@ function genererTableau() {
 
     let soldeTotal = totaux.reduce((acc,val) => acc + val ,0)
     document.getElementById("TotalAll").textContent = `Solde : ${soldeTotal} `;
-    soldeTo = soldeTotal + 233400 ; 
+    soldeTo = soldeTotal + 283400 ; 
     console.log(soldeTo)
     document.getElementById("montantDispo").textContent = `Montant Disponible : ${soldeTo} cfa `
 }
