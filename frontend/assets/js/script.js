da93b65b8a91b83c17d60694524d46bd239a83c0
elbert1509 <a.elbert1509@gmail.com>
2025-04-10 20:20:57 +0200

    15k de Bolingo

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -14,7 +14,7 @@ let membres;
             { nom: "Victoire", photo: "/frontend/assets/images/stephanie.jpg", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Isis", photo: "/frontend/assets/images/isis.jpg", cotisations:[5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Norbert", photo: "/frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-            { nom: "Bolingo", photo: "/frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Bolingo", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Evan's", photo: "/frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Tic-Tac ", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 5000, 5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Naz-K", photo: "/frontend/assets/images/nazk.jpg", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
@@ -35,14 +35,14 @@ let membres;
         ];
     }else {
         membres = [
-            { nom: "Waza", photo: "/frontend/assets/images/waza.jpg", cotisations: [5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Waza", photo: "/frontend/assets/images/waza.jpg", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Melissa", photo: "/frontend/assets/images/melissa.jpg", cotisations: [5000, 5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Victoire", photo: "/frontend/assets/images/stephanie.jpg", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Isis", photo: "/frontend/assets/images/isis.jpg", cotisations:[5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Norbert", photo: "/frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-            { nom: "Bolingo", photo: "/frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Bolingo", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Evan's", photo: "/frontend/assets/images/photo1.png", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-            { nom: "Tic-Tac ", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+            { nom: "Tic-Tac ", photo: "/frontend/assets/images/photo1.png", cotisations: [5000, 5000, 5000, 5000, 5000, 0, 0, 0, 0, 0, 0, 0] },
             { nom: "Naz-K", photo: "/frontend/assets/images/nazk.jpg", cotisations: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
         ]
 
