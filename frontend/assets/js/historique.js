680db1b487510c36173f769e5f837afa21760ac1
elbert1509 <a.elbert1509@gmail.com>
2025-12-30 10:25:12 +0100

    Maj isis

--- frontend/assets/js/historique.js
+++ frontend/assets/js/historique.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         { description: "Cotisation waza", montant: 10000 },  
         { description: "Cotisation Isis 30/10/25", montant: 30000 },  
         { description: "Solde Mélissa 27/12/25 (A vérifier)", montant: 40000 },  
-        { description: "Solde Isis 30/12/25 (A vérifier)", montant: 40000 },  
+        { description: "Solde Isis 30/12/25 (A vérifier)", montant: 20000 },  
     ];
 
     let total = 0;
