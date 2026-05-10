ab2864d0b55f64dfb3c166a0ed5c6cc5f3fbf164
elbert1509 <a.elbert1509@gmail.com>
2026-05-10 15:12:21 +0200

    Maj Stéphanie

--- frontend/assets/js/historique.js
+++ frontend/assets/js/historique.js
@@ -22,7 +22,8 @@ document.addEventListener("DOMContentLoaded", function () {
         { label: "Consultation compte",           montant: -100   },
         { label: "Layette bebe Isis",             montant: -51500 },
         { label: "Cotisation Ernest - 21/03/26",  montant: 5000   },
-        { label: "Cotisation Mélissa - 28/04/26", montant: 15000  }
+        { label: "Cotisation Mélissa - 28/04/26", montant: 15000  },
+        { label: "Cotisation Stephanie - 06/05/26", montant: 15000  },
     ];
 
     var totalCredits = 0;
